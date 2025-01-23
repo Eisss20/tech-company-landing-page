6168b1cc6a649768f1eb442e7d704f97b75f5f70
Kong Thiraphat <thiraphat.cto@gmail.com>
2025-01-23 16:47:29 +0700

    feat: add pricing section component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <>
-      <NavBar/>
-      <HeroSection/>
+      <NavBar />
+      <HeroSection />
     </>
   );
 }
