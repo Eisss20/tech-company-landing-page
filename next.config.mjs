e08d3aae282a054187454c7e673974d9e3880522
Kong Thiraphat <thiraphat.cto@gmail.com>
2025-01-26 19:21:28 +0700

    feat: add promotional component and responsive

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    reactRefresh: false, // ปิดการทำงานของ Fast Refresh
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Promotional.js
@@ -0,0 +1,29 @@
+import styles from "../styles/promotional.module.css";
+import picture from "../../public/assets/picture.svg";
+import Image from "next/image";
+
+function Promotional() {
+  return (
+    <>
+      <section className={styles.PromotionalContainer}>
+        <div className={styles.PromotionalText}>
+          <p>Get Ready to Maximize Your Productivity</p>
+          <p>With Our Workflow Solutions</p>
+        </div>
+        <button className={styles.PromotionalButton}>Get Started</button>
+        <Image
+          src={picture}
+          alt="Background"
+          style={{
+            objectFit: "cover",
+          }}
+          quality={100}
+          className={styles.PromotionalImage}
+        />
+        <div className={styles.stripe}></div>
+      </section>
+    </>
+  );
+}
+
+export default Promotional;
